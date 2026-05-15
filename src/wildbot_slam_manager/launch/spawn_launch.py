91b68a9d5f3f4a72f38861c149fae0d55738d954
TWang <wang.tw.ted@gmail.com>
2026-05-16 03:51:24 +0800

    update

--- src/wildbot_slam_manager/launch/spawn_launch.py
+++ src/wildbot_slam_manager/launch/spawn_launch.py
@@ -2,37 +2,24 @@ import os
 import xacro
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, PythonExpression
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 from launch.conditions import IfCondition
 
-def generate_launch_description():
-    localization_pkg = os.path.join('/home/wildbot/wildbot_slam_ws/src/wildbot_slam_manager')
-    sllidar_pkg = get_package_share_directory('rplidar_ros')
+def generate_launch_description():    
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
     localization_mode = LaunchConfiguration('localization_mode', default='mapping')
-    # map_yaml_file = os.path.join(localization_pkg, 'maps', 'slam_map_0.yaml')
     
-    xacro_file = os.path.join(localization_pkg, 'urdf', 'sensors.urdf.xacro')
-    robot_description_raw = xacro.process_file(xacro_file).toxml()
-
-    robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
+    ekf_node = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node',
         output='screen',
-        parameters=[{
-            'robot_description': robot_description_raw,
-            'use_sim_time': use_sim_time
-        }]
-    )
-
-    # start RPLiDAR S3
-    lidar_node = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(sllidar_pkg, 'launch', 'sllidar_s3_launch.py')),
-        launch_arguments={'serial_port': '/dev/ttyUSB0', 'frame_id': 'laser', 'inverted': 'true'}.items()
+        parameters=[PathJoinSubstitution([FindPackageShare('wildbot_slam_manager'), 'config', 'ekf_config.yaml']),
+            {'use_sim_time': use_sim_time}]
     )
 
     mapping_node = Node(
@@ -60,54 +47,11 @@ def generate_launch_description():
         ]
     )
 
-    # Cartographer mapping
-    cartographer_mapping_node = Node(
-        condition=IfCondition(PythonExpression(["'", localization_mode, "' == 'carto_mapping'"])),
-        package='cartographer_ros',
-        executable='cartographer_node',
-        name='cartographer_node',
-        output='screen',
-        parameters=[{'use_sim_time': use_sim_time}],
-        arguments=[
-            '-configuration_directory', os.path.join(localization_pkg, 'cartographer_config'),
-            '-configuration_basename', 'cartographer_2d.lua'
-        ]
-    )
-    # Convert Submap to OccupancyGrid
-    occupancy_grid_node = Node(
-        condition=IfCondition(PythonExpression(["'", localization_mode, "' in ['carto_mapping', 'cartographer']"])),
-        package='cartographer_ros',
-        executable='cartographer_occupancy_grid_node',
-        name='cartographer_occupancy_grid_node',
-        output='screen',
-        parameters=[{'use_sim_time': use_sim_time}],
-        arguments=['-resolution', '0.05']
-    )
-    # Cartographer localization
-    cartographer_node = Node(
-        condition=IfCondition(PythonExpression(["'", localization_mode, "' == 'cartographer'"])),
-        package='cartographer_ros',
-        executable='cartographer_node',
-        name='cartographer_node',
-        output='screen',
-        parameters=[{'use_sim_time': use_sim_time}],
-        arguments=[
-            '-configuration_directory', os.path.join(localization_pkg, 'cartographer_config'),
-            '-configuration_basename', 'localization.lua',
-            '-load_state_filename', os.path.join(localization_pkg, 'maps', 'wildbot_map_0.pbstream')
-        ],
-    )
-
     return LaunchDescription([
-        DeclareLaunchArgument('use_sim_time', default_value='false'),
+        DeclareLaunchArgument('use_sim_time', default_value='true'),
         DeclareLaunchArgument('localization_mode', default_value='mapping'),
 
-        robot_state_publisher,
-        lidar_node,
+        ekf_node,
         mapping_node,
-        localization_node,
-
-        cartographer_mapping_node,
-        occupancy_grid_node,
-        cartographer_node
+        localization_node
     ])
